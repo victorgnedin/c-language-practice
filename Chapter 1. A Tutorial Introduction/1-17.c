ae6772c8c6cfba5b8ed7e93c7c4ab24a45c180d5
Victor Gnedin <gnedinvs@gmail.com>
2020-03-30 15:25:30 +0200

    Add exercise 1-17

---
+++ Chapter 1. A Tutorial Introduction/1-17.c
@@ -0,0 +1,54 @@
+/* Exercise 1-17. Write a program to print all input lines that are longer than 80 characters */
+
+#include <stdio.h>
+
+#define MAX_LENGTH 1000    /* maximum input line size */
+#define PRINT_FROM 80      /* longer lines are printed */
+#define MAX_LINES  100     /* maximum lines (PRINT_FROM + 1 characters) storage */
+
+int getline_custom(char s[], int lim);    /* read a line into s, return length */
+void copy(char to[], char from[]);        /* copy 'from' into 'to'; assume to is big enough */
+
+int main()
+{
+    char cur_line[MAX_LENGTH];    /* current input line */
+    int cur_line_length;          /* current input line length */
+
+    char out[MAX_LINES][MAX_LENGTH];    /* storage for lines (PRINT_FROM + 1 characters) to output */
+    int out_lines_count = 0;            /* indexer for out[][] lines, also controls overflow */
+
+    while (out_lines_count < MAX_LINES && (cur_line_length = getline_custom(cur_line, MAX_LENGTH)) > 0)
+        if (cur_line_length > PRINT_FROM) {
+            copy(out[out_lines_count], cur_line);
+            ++out_lines_count;
+        }
+    
+    int i;
+    for (i = 0; i < out_lines_count; ++i)
+        printf("%s", out[i]);
+    printf("\n");
+
+    return 0;
+}
+
+int getline_custom(char s[], int lim)
+{
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+void copy(char to[], char from[])
+{
+   int i = 0;
+
+   while ((to[i] = from[i]) != '\0')
+       ++i;
+}
